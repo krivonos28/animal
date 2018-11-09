bbedf421647baf88b7b154619888b8dd404991d3
Krivonos Aleksandr <krivonosaleksandr@gmail.com>
2018-11-09 16:44:30 +0300

    app with routing

--- src/Components/AddAnimalForm.js
+++ src/Components/AddAnimalForm.js
@@ -1,13 +1,45 @@
 import React, {Component} from 'react'
-
+import{Link} from "react-router-dom"
+import axios from 'axios'
 
 class AddAnimalForm extends Component{
 constructor(props){
     super()
+
 } 
+postAnimalOnServer(e) {
+    console.time( 'srat')
+    e.preventDefault()
+    console.log(document.querySelector('#nickname').value)
+    console.log(document.querySelector('#price').value)
+    console.log(document.querySelector('#age').value)
+    console.log(document.querySelector('#input-type').value)
+    console.timeEnd('end');
+    axios.post('http://localhost:3012/animals', {
+      nickname: (document.querySelector('#nickname').value).toString(),
+      price: (document.querySelector('#price').value).toString(),
+      age: (document.querySelector('#age').value).toString(),
+      type: (document.querySelector('#input-type').value).toString()
+    })
+      .then((response) => {
+        console.log('hey', response);
+        this.allAnimals.push(response.data);
+        this.setState({counter: this.state.counter + 1})
+        })
+     
+    document.querySelector('#nickname').value = '';
+    document.querySelector('#price').value = '';
+    document.querySelector('#age').value = '';
+    document.querySelector('#input-type').value = '';
+    
+  }
 render(){
-    return (<div className="container">
-        <form onSubmit ={this.props.postAnimal}>
+    return (
+        <div>
+    <Link to='/'>Home</Link>
+    
+    <div className="container">
+        <form onSubmit ={this.postAnimalOnServer.bind(this)}>
             <div className="form-row">
                 <div className="form-group col-3">
                     <label htmlFor="nickname">Nickname</label>
@@ -34,7 +66,7 @@ render(){
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
 
-    </div>
+    </div></div>
 
     )
 }
